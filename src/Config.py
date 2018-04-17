02646d2a72ad9db7b6fe6b9ea12e75ee7d1f2635
Mpho Mphego <mpho112@gmail.com>
2018-04-17 12:17:32 +0200

    Added somewhat config file

---
+++ src/Config.py
@@ -0,0 +1,9 @@
+title = "CBF Sensors Dashboard"
+metadata = 'charset="UTF-8" http-equiv="refresh" content="5"'
+# JS links
+js_link = {
+            "codepen": 'https://codepen.io/mmphego/pen/KoJoZq.js',
+            "jquery": "http://code.jquery.com/jquery-1.10.1.min.js"
+            }
+# CSS links
+css_link = 'https://codepen.io/mmphego/pen/KoJoZq.css'
